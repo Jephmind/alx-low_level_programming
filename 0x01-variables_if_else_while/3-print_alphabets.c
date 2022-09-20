24036e2815cff4a266b1c709222dccdfe800337e
jephthah  Efekiruo <111095633+Jephmind@users.noreply.github.com>
2022-09-21 00:18:06 +0100

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*Declaring variables*/
+	char i;
+	
+	for (i = 'a' ; i <= 'z' ; i++)
+	{/*printing lowercase*/
+	putchar(i);
+	}
+	for (i = 'A' ; i <= 'Z' ; i++)
+	{/*printing uppercase*/
+	putchar(i);
+	}
+	/*printing new line and return*/
+	putchar('\n');
+	return (0);
+}
