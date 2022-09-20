fa8049920140d60fdf98f5bb61b071b0909c670b
jephthah  Efekiruo <111095633+Jephmind@users.noreply.github.com>
2022-09-20 22:56:38 +0100

    return (last_digit);

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function to print last digit
+ * @c: uses the argument
+ * Return: Always 0
+ */
+int print_last_digit(int c)
+{   
+       int last_digit = c % 10;
+       
+        if (last_digit < 0) 
+        {
+           last_digit *= -1;
+           _putchar(last_digit + '0');
+           return (last_digit);
+         }
+         return (0);
+  }
