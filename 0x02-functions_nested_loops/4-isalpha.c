6361174ddadc07b7ea21cea03e1be85f6080cbe1
jephthah  Efekiruo <111095633+Jephmind@users.noreply.github.com>
2022-09-21 13:53:16 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,16 +3,16 @@
 /**
  * _isalpha - function to return 1 if c is an alphabet
  * @c: int that determines argument
-  * Return: Always 0
-  */
+ * Return: Always 0
+ */
 int _isalpha(int c)
 {
-      if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-      {
-            return (1);
-      }
-      else
-      {
-           return (0);
-      }
-} 
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
