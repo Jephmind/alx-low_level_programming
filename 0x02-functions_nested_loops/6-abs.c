efacf13aa3041ab512efe5e7d921df07eb58e509
jephthah  Efekiruo <111095633+Jephmind@users.noreply.github.com>
2022-09-21 13:56:36 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,16 +4,15 @@
  * _abs - prints absolute value of an int
  * @c: is the int that uses the argument function
  * Return: Always 0
-  */
+ */
 int _abs(int c)
 {
-      if (c > 0 || c == 0)
-      {
-        
-           return (c);
-      }
-      else
-      {
-           return (c * -1); 
-      }
-} 
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
