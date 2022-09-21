2db8c94462ac6a2b5c263b9622ddb20812b05b33
jephthah  Efekiruo <111095633+Jephmind@users.noreply.github.com>
2022-09-21 14:31:01 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-
 /**
- * main - Entry point for the numbers
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Description: A program that prints all possible combination of single digits
+ * Return: 0
  */
 int main(void)
 {
-	/*delcare variable*/
-	int i;
+	int a;
 
-	for (i =48; i < 58; i++)
+	for (a = 0; a < 10; a++)
 	{
-	putchar(i);
-	if (i != 57)
+		putchar(a + '0');
+
+		if (a != 9)
 		{
-		putchar(',');
-		putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
