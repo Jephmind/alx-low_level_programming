f5798c6bfd8a7bd7edc5a7b20fc7815ad34b35bd
jephthah  Efekiruo <111095633+Jephmind@users.noreply.github.com>
2022-09-21 13:49:49 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,15 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    char j;
-  
-    for (i = 0; i < 10; i++)
-    {
-            for (j = 'a'; j <= 'z'; j++)
-            {
-                   _putchar(j);
-            }
-            _putchar('\n');
-    }
-} 
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
