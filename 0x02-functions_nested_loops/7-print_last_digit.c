60a427a4eb915e39a9f785f949d32668c3f47941
jephthah  Efekiruo <111095633+Jephmind@users.noreply.github.com>
2022-09-21 13:57:46 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,14 @@
  * Return: Always 0
  */
 int print_last_digit(int c)
-{   
-       int last_digit = c % 10;
-       
-        if (last_digit < 0) 
-        {
-           last_digit *= -1;
-           _putchar(last_digit + '0');
-           return (last_digit);
-         }
-         return (0);
-  }
+{
+	int last_digit = c % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	return (0);
+}
