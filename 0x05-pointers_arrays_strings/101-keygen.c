e0df8ac5f40cdfc875e37dacd060ee5eff8c9b30
jephthah  Efekiruo <111095633+Jephmind@users.noreply.github.com>
2022-09-24 15:02:43 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,23 +3,34 @@
 #include <time.h>
 
 /**
- * main - generates keygen.
- * Return: 0 always.
- */
+*main - program that generates random password
+*
+*Return : Always 0
+*/
+
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+	int	pass_n, count, max_n, min_n;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
+	count = 0;
+	min_n = 32;
+	max_n = 126;
+	while (count < 2772)
 	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
+		srand(time(0) + count);
+		pass_n = (rand() % (max_n - min_n + 1)) + min_n;
+		if ((count + pass_n) <= 2772)
+		{
+			printf("%c", pass_n);
+			count = count + pass_n;
+		}
+		else
 			break;
-		c = c + r;
-		printf("%c", r);
 	}
-	printf("%c\n", (2772 - c));
+	if ( count < 2772)
+	{
+		pass_n = 2772 - count;
+		printf("%c", pass_n);
+	}
 	return (0);
 }
