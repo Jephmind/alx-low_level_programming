d427e07d35d24efe8db5ce3bb0693329fd686dcc
jephthah  Efekiruo <111095633+Jephmind@users.noreply.github.com>
2022-09-24 15:08:16 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _atoi - converts a string to integer
- * @s: input string.
- * Return: integer.
+ * _atoi -  convert a string to an integer
+ * @s: character to check
+ * Return: 0 If there are no numbers in the string
  */
+
 int _atoi(char *s)
 {
-	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	int sn = 1;
 
-	while (*(s + count) != '\0')
-	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+	unsigned int n = 0;
+
+	do {
+		if (*s == '-')
+
+			sn *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+
+			n = (n * 10) + (*s - '0');
+
+		else if (n > 0)
+
 			break;
 
-		if (*(s + count) == '-')
-			pn *= -1;
+	} while (*s++);
 
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
-		{
-			if (size > 0)
-				m *= 10;
-			size++;
-		}
-		count++;
-	}
-
-	for (i = count - size; i < count; i++)
-	{
-		oi = oi + ((*(s + i) - 48) * m);
-		m /= 10;
-	}
-	return (oi * pn);
+	return (n * sn);
 }
